621469a08edf5031d3e357309987f4544ae70dbf
GK <gk@GKs-MacBook-Pro.local>
2025-05-15 22:37:48 +0100

    lateest jsx asat

--- app/components/ButtonLogin.js
+++ app/components/ButtonLogin.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
 
-const ButtonLogin = ({ isloggedin, name, age, children}) => {
-    //let { isloggedin, name, age } = props;
-    if (isloggedin)
-        return (
-            <Link href="/dashboard" className="btn btn-primary text-white">Welcome back {name}</Link>
-        );
-    else
-        return (<p>Maaan, you need to be logged in</p>);
+const ButtonLogin = ({ isloggedin, name, extraStyle }) => {
+  //let { isloggedin, name, age } = props;
+  if (isloggedin)
+    return (
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
+        Welcome back {name}
+      </Link>
+    );
+  else return <p>Maaan, you need to be logged in</p>;
 };
 
 export default ButtonLogin;
--- app/page.js
+++ app/page.js
@@ -1,16 +1,24 @@
 import ButtonLogin from "./components/ButtonLogin";
+import FAQListItem from "./components/FAQListItem";
 
 export default function Home() {
   let isloggedin = true;
   let name = "Icho";
+  const pricingList = [
+    "Cellect Customer Feedback",
+    "Unlimited Boards",
+    "Admin Dashboard",
+    "24/7 Support",
+  ];
   return (
     <main>
+      {/* header */}
       <section className="bg-base-300">
         <div className="max-w-3xl m-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">IchoSolutions</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">Pricing</a>
+            <a className="link link-hover" href="#faq">FAQ</a>
           </div>
           <div>
             <ButtonLogin isloggedin={isloggedin} name={name} />
@@ -18,17 +26,96 @@ export default function Home() {
         </div>
       </section>
 
-      {/* _______________________________________*/}
+      {/* hero */}
 
       <section className="text-center py-32 px-8 max-w-3xl m-auto">
         <h1 className="text-4xl font-extrabold mb-6">
-          Cellect customer feedback to build better products!
+          Collect customer feedback to build better products!
         </h1>
         <div className="opacity-90 mb-10">
           Create a feedback board in minutes, prioritize features and build
           products your customers will love.
         </div>
         <ButtonLogin isloggedin={isloggedin} name={name} />
+      </section>
+
+      {/* pricing */}
+      <section className="bg-base-200" id="pricing">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase text-primary font-medium mb-4 text-center">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+            <ul className="space-y-2">
+              {pricingList.map((item) => (
+                <li className="flex gap-2 items-center" key={item}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="text-green-600 size-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 12.75 6 6 9-13.5"
+                    />
+                  </svg>
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+            <ButtonLogin
+              isloggedin={isloggedin}
+              name={name}
+              extraStyle="w-full"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* FAQ */}
+      <section className="bg-base-200" id="faq">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase text-primary font-medium mb-4 text-center">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+          <ul className="max-w-lg mx-auto">
+          {
+            [
+              {
+                question: "Are you there icho?", 
+                answer: "You better believe it"
+              },
+              {
+                question: "How do i get a refund?", 
+                answer: "No money back! this is drarga people buddy!"
+              },
+              {
+                question: "What do i get exactly?", 
+                answer: "You get your ass handed to you in here! this is Drarga buddy!"
+              },
+            ].map((qa) => (
+              <FAQListItem key={qa.question} qa={qa}/>
+            ))
+          }
+          </ul>
+        </div>
       </section>
     </main>
   );
